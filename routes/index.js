9840d175dc2f6a4564cd28e9cefae67327306ae3
Joseph Cannova <jcannova@wheelership.com>
2019-05-15 22:38:43 -0400

    Added functions to scrap, insert into db, delete collection.  Created routes that call the appropriate function.  Transfered data into views.  Added bootstrap.

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,58 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const axios = require('axios');
-const ArticlesModel = require('../db/models/Articles');
-//TODO: Continue with the Articles Const Line20
-
+const axios = require("axios");
+const ArticlesModel = require("../db/models/Articles");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
+  ArticlesModel.find({}).then(data => {
+    res.render("index", { data: data });
+  });
+});
 
-  let url = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.NYTIMES_API_KEY}`;
+/**Scrap for new news */
+router.get("/add", function(req, res, next) {
+  fetchTimesDataFromApi(data => {
+    insertIntoMongo(ArticlesModel, data);
+    res.redirect("/");
+  });
+});
+
+router.delete("/", function (req, res, next){
+  ArticlesModel.remove({}, response => {
+    console.log(response); 
+    res.redirect("/");
+}
+)});
+
+module.exports = router;
+
+/**Functions */
+
+function fetchTimesDataFromApi(cb) {
+  let url = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${
+    process.env.NYTIMES_API_KEY
+  }`;
   console.log(url);
 
   axios({
-    method:'get',
-    url: url,
-  }).then( response => {
-    //console.log(data.data);
-    let data = response.data.results;
-    const articles = new ArticlesModel()
-   
-
-    data.forEach(element => {
-      console.log("TITLE: ", element.title);
-      console.log("DESC: ", element.abstract);
-      console.log("SHORTURL: ", element.short_url);
-    });
-    
-  }).catch(err => {
-    console.log(err.data);
-
+    method: "get",
+    url: url
   })
-  
+    .then(response => {
+      return cb(response.data.results);
+    })
+    .catch(err => {
+      return cb(err);
+    });
+}
 
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+async function insertIntoMongo(collection, data) {
+  await data.forEach(element => {
+    ArticlesModel.create({
+      title: element.title,
+      desc: element.abstract,
+      url: element.short_url
+    });
+  });
+}
